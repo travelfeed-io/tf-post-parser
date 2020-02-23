1cb0f08dfd4d20b04fe51c16f414a481a1d243e8
tiotdev <tiotdev@tiot.de>
2020-02-23 21:33:08 +0530

    Export sanitizeHtmlConfig

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const { imageProxy } = require('./getImage');
 const { parseBody } = require('./parseBody');
+const { sanitizeHtmlConfig } = require('./sanitizeConfig');
 
-module.exports = { parseBody, imageProxy };
+module.exports = { parseBody, imageProxy, sanitizeHtmlConfig };
