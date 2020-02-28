8cd610fda9625d6b1684afae8d633f4e7012357d
tiotdev <tiotdev@tiot.de>
2020-02-28 15:04:22 +0530

    Fix

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const { imageProxy } = require('./getImage');
 const { parseBody } = require('./parseBody');
 const { sanitizeHtmlConfig } = require('./sanitizeConfig');
 const { cleanTags, photoTags, removeTags } = require('./cleanTags');
-const { processBody } = require('./cleanTags');
+const { processBody } = require('./processBody');
 const { getPostLocation } = require('./getPostLocation');
 const { communities, communityList, getCommunity } = require('./communities');
 
