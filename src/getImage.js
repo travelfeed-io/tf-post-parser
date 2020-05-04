0f1b24daaaa1d796b264863bac9b9a2748ae2263
tiotdev <tiotdev@tiot.de>
2020-05-04 11:22:50 +0200

    Try catch imageProxy

--- src/getImage.js
+++ src/getImage.js
@@ -1,18 +1,24 @@
 const bs58 = require('bs58');
 
 const imageProxy = (imgUrl, width, height, mode, format) => {
-  if (imgUrl === '') {
+  if (!imgUrl) {
     return undefined;
   }
-  // Base58 encode image url
-  // https://github.com/steemit/imagehoster
-  const bytes = Buffer.from(imgUrl);
-  const address = bs58.encode(bytes);
-  // Use webp as format for best compression if supported
-  // Get the cropped steemitimages URL for an image
-  return `https://steemitimages.com/p/${address}/?format=${format || 'match'}${
-    width ? `&width=${width}` : ''
-  }${height ? `&height=${height}` : ''}${mode ? `&mode=${mode}` : ''}`;
+  try {
+    // Base58 encode image url
+    // https://github.com/steemit/imagehoster
+    const bytes = Buffer.from(imgUrl);
+    const address = bs58.encode(bytes);
+    // Use webp as format for best compression if supported
+    // Get the cropped steemitimages URL for an image
+    return `https://steemitimages.com/p/${address}/?format=${format ||
+      'match'}${width ? `&width=${width}` : ''}${
+      height ? `&height=${height}` : ''
+    }${mode ? `&mode=${mode}` : ''}`;
+  } catch (err) {
+    console.warn(err);
+    return undefined;
+  }
 };
 
 module.exports = { imageProxy };
