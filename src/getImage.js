a0fd2b222d3652de64ee215c97145c4cf39f1e58
tiotdev <tiotdev@tiot.de>
2020-08-14 00:01:16 +0200

    Use steemitimages url for unknown origins (for caching old images deleted from origin)

--- src/getImage.js
+++ src/getImage.js
@@ -1,10 +1,26 @@
 const bs58 = require('bs58');
 
-const imageProxy = (imgUrl, width, height, mode, format) => {
-  if (!imgUrl) {
+const imageProxy = (url, width, height, mode, format) => {
+  if (!url) {
     return undefined;
   }
   try {
+    let imgUrl = url;
+    if (
+      !imgUrl.match(/img\.travelfeed\.io/) &&
+      !imgUrl.match(/steemitimages\.com/) &&
+      !imgUrl.match(/files\.steempeak\.com/) &&
+      !imgUrl.match(/files\.peakd\.com/) &&
+      !imgUrl.match(/images\.hive\.blog/)
+    ) {
+      // Steemitimages needs to be used because it has a cache of images deleted from the origin
+      // Base58 encode image url
+      // https://github.com/steemit/imagehoster
+      const bytes = Buffer.from(imgUrl);
+      const address = bs58.encode(bytes);
+      // Get the cropped steemitimages URL for an image
+      imgUrl = `https://steemitimages.com/p/${address}/?format=match&mode=fit`;
+    }
     // Base58 encode image url
     // https://github.com/steemit/imagehoster
     const bytes = Buffer.from(imgUrl);
