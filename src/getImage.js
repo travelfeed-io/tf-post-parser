1d62d4b9cdbd39e228ea442a0f3e401fa343b6a2
tiotdev <tiotdev@tiot.de>
2024-05-29 23:57:04 +0200

    Update getImage.js

--- src/getImage.js
+++ src/getImage.js
@@ -29,7 +29,8 @@ const imageProxy = (url, width, height, mode, format) => {
       !imgUrl.match(/images\.hive\.blog/) &&
       !imgUrl.match(/images\.unsplash\.com/) &&
       !imgUrl.match(/img\.esteem\.app/) &&
-      !imgUrl.match(/images\.ecency\.com/)
+      !imgUrl.match(/images\.ecency\.com/) &&
+      !imgUrl.match(/ipfs-3speak\.b-cdn\.net/)
     ) {
       // Steemitimages needs to be used because it has a cache of images deleted from the origin
       // Base58 encode image url
